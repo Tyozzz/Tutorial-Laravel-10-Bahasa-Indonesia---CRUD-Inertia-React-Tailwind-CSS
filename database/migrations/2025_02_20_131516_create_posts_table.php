20e0b06a91fecd82bcc3878ff72ecb3957ddaef5
Tyozzz <satrionugroho182008@gmail.com>
2025-02-20 20:18:25 +0700

    Mengubah isi codingan create post model

--- database/migrations/2025_02_20_131516_create_posts_table.php
+++ database/migrations/2025_02_20_131516_create_posts_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('posts');
     }
 };
+
