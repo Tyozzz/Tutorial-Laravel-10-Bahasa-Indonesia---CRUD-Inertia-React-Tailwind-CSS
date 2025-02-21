681f7cef524a3112377a8ee633605bd20d88dc4d
Tyozzz <satrionugroho182008@gmail.com>
2025-02-21 20:48:12 +0700

    update

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
+use Illuminate\Support\Facades\Auth; 
 
 class HandleInertiaRequests extends Middleware
 {
@@ -30,16 +31,27 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = Auth::user();
+
+        if ($user) {
+            $user->load('roles');
+
+            $user = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'roles' => $user->roles,
+            ];
+        }
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
             ],
             'ziggy' => function () use ($request) {
-    return array_merge((new Ziggy)->toArray(), [
-        'location' => $request->url(),
-    ]);
-},
-
+                return array_merge((new Ziggy)->toArray(), [
+                    'location' => $request->url(),
+                ]);
+            },
             'flash' => function () use ($request) {
                 return [
                     'success' => $request->session()->get('success'),
