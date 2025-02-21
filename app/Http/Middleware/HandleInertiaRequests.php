ab2895cb58740860e0f2e12d5ad0ef70a932e2f1
Tyozzz <satrionugroho182008@gmail.com>
2025-02-21 14:00:32 +0700

    Mengubah isi codingan handler

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
-use Tightenco\Ziggy\Ziggy;
+use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -35,10 +35,11 @@ class HandleInertiaRequests extends Middleware
                 'user' => $request->user(),
             ],
             'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
-                    'location' => $request->url(),
-                ]);
-            },
+    return array_merge((new Ziggy)->toArray(), [
+        'location' => $request->url(),
+    ]);
+},
+
             'flash' => function () use ($request) {
                 return [
                     'success' => $request->session()->get('success'),
